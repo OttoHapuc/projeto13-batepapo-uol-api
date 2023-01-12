a513f6339cc50ca03b6afd2df60d56e0abc794e7
Ottoniel Hapuc <dossantoshapuc@gmail.com>
2023-01-12 16:15:23 -0300

    salva no mongo usuário com time para calculo de tempo e salva mensagem com time corretamente, faltam processos de validação e seguintes tópics

--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
 import cors from 'cors';
 import express from 'express';
-
+import Joi from "joi";
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+import dayjs from 'dayjs';
+dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = 5000;
 
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+mongoClient.connect()
+    .then(() => {
+        db = mongoClient.db("trabalhoUOL");
+    })
+    .catch(() => {
+        console.log("error connecting to MongoDB server")
+    });
 
+app.get('/participants', (req, res) => {
+    db.collection("users")
+        .find()
+        .toArray()
+        .then(dados => res.send(dados)
+            .catch(err => res.status(500).send(err)));
+})
+
+app.post("/participants", (req, res) => {
+    const { name } = req.body;
+    console.log(name);
+
+    try {
+        const userExist = db.collection("users").findOne({name})
+        if(userExist) return res.status(409).send("user conflict")
+        db.collection("users").insertOne({ name, lastStatus: Date.now() })
+        res.status(201).send("OK")
+    } catch (err){ res.status(500)};
+});
+
+app.post("/messages", (req,res) => {
+    const {to, text, type} = req.body;
+    const {from} = req.headers;
+
+    try {
+        db.collection("messages").insertOne({ from,to,text,type, time: dayjs().format("HH:mm:ss") })
+        res.status(201).send("OK")
+    } catch (err){ res.status(500)};
+})
 
 app.listen(PORT);
